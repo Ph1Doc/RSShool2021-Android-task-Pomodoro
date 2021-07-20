4ff1bf9fbb27f70e369b1a054048259cc2e12dc1
Ph1Doc <oleg.sapun@gmail.com>
2021-07-20 23:01:22 +0300

    feat: add timer

--- app/src/main/java/com/example/pomodoro/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pomodoro
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,12 +10,20 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pomodoro.databinding.ActivityMainBinding
+import com.example.pomodoro.stopwatch.Stopwatch
+import com.example.pomodoro.stopwatch.StopwatchAdapter
+import com.example.pomodoro.stopwatch.StopwatchListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), StopwatchListener {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private val stopwatchAdapter = StopwatchAdapter(this)
+    private val stopwatches = mutableListOf<Stopwatch>()
+    private var nextId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,37 +31,50 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.recycler.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = stopwatchAdapter
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        binding.addNewStopwatchButton.setOnClickListener {
+            if (binding.minutes.text.toString().toIntOrNull() != null ) {
+                stopwatches.add(Stopwatch(nextId++, binding.minutes.text.toString().toLong() * 10 * 60, false))
+                stopwatchAdapter.submitList(stopwatches.toList())
+            } else {
+                Toast.makeText(this, "Enter the time", Toast.LENGTH_SHORT).show()
+            }
         }
+
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    override fun start(id: Int) {
+        changeStopwatch(id, null, true)
+    }
+
+    override fun stop(id: Int, currentMs: Long) {
+        changeStopwatch(id, currentMs, false)
+    }
+
+    override fun reset(id: Int) {
+        changeStopwatch(id, 0L, false)
+    }
+
+    override fun delete(id: Int) {
+        stopwatches.remove(stopwatches.find { it.id == id })
+        stopwatchAdapter.submitList(stopwatches.toList())
+    }
+
+    private fun changeStopwatch(id: Int, currentMs: Long?, isStarted: Boolean) {
+        val newTimers = mutableListOf<Stopwatch>()
+        stopwatches.forEach {
+            if (it.id == id) {
+                newTimers.add(Stopwatch(it.id, currentMs ?: it.currentMs, isStarted))
+            } else {
+                newTimers.add(it)
+            }
+        }
+        stopwatchAdapter.submitList(newTimers)
+        stopwatches.clear()
+        stopwatches.addAll(newTimers)
     }
 }
